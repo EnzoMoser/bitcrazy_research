a41d30e97e5572f0aa8a93a53ddf743665953175
Enzo Moser <irlenzom@gmail.com>
2024-06-26 17:03:14 +0000

    Removed unnecessary file

---
+++ drone_scripts/pos_mov.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+#
+#     ||          ____  _ __
+#  +------+      / __ )(_) /_______________ _____  ___
+#  | 0xBC |     / __  / / __/ ___/ ___/ __ `/_  / / _ \
+#  +------+    / /_/ / / /_/ /__/ /  / /_/ / / /_/  __/
+#   ||  ||    /_____/_/\__/\___/_/   \__,_/ /___/\___/
+#
+#  Copyright (C) 2018 Bitcraze AB
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+"""
+This script shows the basic use of the PositionHlCommander class.
+
+Simple example that connects to the crazyflie at `URI` and runs a
+sequence. This script requires some kind of location system.
+
+The PositionHlCommander uses position setpoints.
+
+Change the URI variable to your Crazyflie configuration.
+"""
+import cflib.crtp
+import time
+from cflib.crazyflie import Crazyflie
+from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
+from cflib.positioning.position_hl_commander import PositionHlCommander
+from cflib.utils import uri_helper
+
+# URI to the Crazyflie to connect to
+uri = uri_helper.uri_from_env(default='radio://0/80/2M/EE5C21CFA6')
+
+def simple_sequence():
+    with SyncCrazyflie(uri, cf=Crazyflie(rw_cache='./cache')) as scf:
+        with PositionHlCommander(scf, controller=PositionHlCommander.CONTROLLER_PID) as pc:
+            for _ in range(2):
+                pc.forward(0.5)
+                pc.left(0.5)
+                pc.back(0.5)
+                pc.right(0.5)
+                time.sleep(2)
+                pc.forward(0.5)
+                pc.back(0.5)
+                time.sleep(2)
+                pc.go_to(0.5, 0.5, 1.0)
+                time.sleep(2)
+                pc.go_to(0.0, 0.0, 0.5)
+                time.sleep(2)
+                pc.go_to(0.0, 0.0, 0.3)
+
+
+if __name__ == '__main__':
+    cflib.crtp.init_drivers()
+
+    simple_sequence()
